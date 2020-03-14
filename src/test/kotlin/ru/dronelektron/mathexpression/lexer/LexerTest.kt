d025c8a5f2b821478351c7c472478da4c4fd22ea
Dron-elektron <dron.elektron@gmail.com>
2020-03-14 14:26:49 +0300

    Add "scanGood" method for tests

--- src/test/kotlin/ru/dronelektron/mathexpression/lexer/LexerTest.kt
+++ src/test/kotlin/ru/dronelektron/mathexpression/lexer/LexerTest.kt
@@ -7,105 +7,87 @@ import ru.dronelektron.mathexpression.lexer.TokenType.*
 class LexerTest {
 	@Test
 	fun tokens_whenEmptyInput_returnsEof() {
-		val lexer = Lexer("")
-
 		val expectedTokens = listOf(
 			Token(EOF, "", 0)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood("", expectedTokens)
 	}
 
 	@Test
 	fun tokens_whenLeftParenAndRightParen_returnsBoth() {
-		val lexer = Lexer("()")
-
 		val expectedTokens = listOf(
 			Token(LEFT_PAREN, "(", 0),
 			Token(RIGHT_PAREN, ")", 1),
 			Token(EOF, "", 2)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood("()", expectedTokens)
 	}
 
 	@Test
 	fun tokens_whenComma_returnsIt() {
-		val lexer = Lexer(",")
-
 		val expectedTokens = listOf(
 			Token(COMMA, ",", 0),
 			Token(EOF, "", 1)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood(",", expectedTokens)
 	}
 
 	@Test
 	fun tokens_whenConstantWithoutPoint_returnsIt() {
-		val lexer = Lexer("2")
-
 		val expectedTokens = listOf(
 			Token(CONSTANT, "2", 0),
 			Token(EOF, "", 1)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood("2", expectedTokens)
 	}
 
 	@Test
 	fun tokens_whenConstantWithPoint_returnsIt() {
-		val lexer = Lexer("2.4")
-
 		val expectedTokens = listOf(
 			Token(CONSTANT, "2.4", 0),
 			Token(EOF, "", 3)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood("2.4", expectedTokens)
 	}
 
 	@Test
 	fun tokens_whenConstantStartsWithPoint_returnsIt() {
-		val lexer = Lexer(".4")
-
 		val expectedTokens = listOf(
 			Token(CONSTANT, ".4", 0),
 			Token(EOF, "", 2)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood(".4", expectedTokens)
 	}
 
 	@Test
 	fun tokens_whenSecondPointAfterConstantIsDetected_shouldBeIgnored() {
-		val lexer = Lexer("2.4.3")
-
 		val expectedTokens = listOf(
 			Token(CONSTANT, "2.4", 0),
 			Token(CONSTANT, ".3", 3),
 			Token(EOF, "", 5)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood("2.4.3", expectedTokens)
 	}
 
 	@Test
 	fun tokens_whenIdentifier_returnsIt() {
-		val lexer = Lexer("eee_EEE_24")
-
 		val expectedTokens = listOf(
 			Token(IDENTIFIER, "eee_EEE_24", 0),
 			Token(EOF, "", 10)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood("eee_EEE_24", expectedTokens)
 	}
 
 	@Test
 	fun tokens_whenOperations_returnsAll() {
-		val lexer = Lexer("+ - * / ^")
-
 		val expectedTokens = listOf(
 			Token(ADDITION, "+", 0),
 			Token(SUBTRACTION, "-", 2),
@@ -115,7 +97,7 @@ class LexerTest {
 			Token(EOF, "", 9)
 		)
 
-		assertEquals(expectedTokens, lexer.tokens)
+		scanGood("+ - * / ^", expectedTokens)
 	}
 
 	@Test
@@ -146,6 +128,12 @@ class LexerTest {
 	@Test
 	fun tokens_whenIncompleteConstant_throwsException() {
 		scanBad(".", Lexer.ERROR_UNKNOWN_TOKEN, 0)
+	}
+
+	private fun scanGood(expressionText: String, expectedTokens: List<Token>) {
+		val lexer = Lexer(expressionText)
+
+		assertEquals(expectedTokens, lexer.tokens)
 	}
 
 	private fun scanBad(expressionText: String, expectedMessage: String, expectedPosition: Int) {
